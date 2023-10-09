539d60ddc6d9607a43ab2c2dc9bd727e6209eca7
Cybercom682 <johannes.fischer58@berufsschulebt1.onmicrosoft.com>
2023-10-09 10:54:21 +0200

    add xmlHelper | SmartyFunc

---
+++ admin/controller/tools/smartyFuncs.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace tools;
+
+class smartyFuncs
+{
+
+}
--- includes/controller/xmlHelper.php
+++ includes/controller/xmlHelper.php
@@ -1,6 +1,6 @@
 <?php
 
-class XmlReader
+class xmlHelper
 {
     public function readFromXml(string $file, int $returnType)
     {
